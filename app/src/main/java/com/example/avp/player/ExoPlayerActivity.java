1f7cf8b8956303a9a1f947b7b7eefc65d6cdbe1b
DF5HSE <fild10@yandex.ru>
2021-05-20 23:09:37 +0300

    Add own player and fix some bags
    Rewind and forward increment implemented with buttons.
    Also added lock rotation button and progress bar

--- app/src/main/java/com/example/avp/player/ExoPlayerActivity.java
+++ app/src/main/java/com/example/avp/player/ExoPlayerActivity.java
@@ -3,25 +3,39 @@ package com.example.avp.player;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.example.avp.R;
 import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.ui.PlayerView;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.Objects;
 
 public class ExoPlayerActivity extends AppCompatActivity {
     PlayerView playerView;
+    ProgressBar progressBar;
+    LinearLayout centerPlayerControl;            // pause, play buttons
+    ImageView lockRotationButton;
+    SimpleExoPlayer player;
+
+    static final Handler handler = new Handler(); // for making delay for set visibility of progressBar etc
+    static final int hideBarsFlags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,25 +46,31 @@ public class ExoPlayerActivity extends AppCompatActivity {
         if (linkOnVideo.isEmpty()) {
             linkOnVideo = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
         }
+
         createSimpleExoPlayerAndPlayVideoByLink(linkOnVideo);
     }
 
     private void createSimpleExoPlayerAndPlayVideoByLink(String linkOnVideo) {
-        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
-        playerView = (PlayerView) findViewById(R.id.playerView);
+        // assign variables
+        playerView = findViewById(R.id.player_view);
+        progressBar = findViewById(R.id.progress_bar);
+        centerPlayerControl = findViewById(R.id.center_player_control);
+        lockRotationButton = findViewById(R.id.lock_rotation);
+
+        // make activity full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        player = new SimpleExoPlayer.Builder(this).build();
         playerView.setPlayer(player);
 
         // hide title bar
-        if (getSupportActionBar()!= null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
         }
 
         // hide notification bar and set full screen
-        playerView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | // hide navigation line on the bottom of screen (that helps go home)
-                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY /* hide notification bar and show it only after sliding down,
-                                                                                         and hide again in some seconds */
-        );
+        playerView.setSystemUiVisibility(hideBarsFlags);
 
         /* TODO
          if (isLinkOnGoogleDrive(linkOnVideo)) {
@@ -64,7 +84,53 @@ public class ExoPlayerActivity extends AppCompatActivity {
         player.setMediaItem(MediaItem.fromUri(Uri.parse(linkOnVideo)));
 
         player.prepare();
-        player.play();
+
+        player.setPlayWhenReady(true);
+        player.addListener(new Player.EventListener() {
+            @Override
+            public void onPlaybackStateChanged(int state) {
+                if (state == Player.STATE_BUFFERING) {
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // Do something after 5s = 5000ms
+                            if (player.getPlaybackState() == Player.STATE_BUFFERING) {
+                                progressBar.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }, 500);
+                } else if (state == Player.STATE_READY) {
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lockRotationButton.setOnClickListener(new View.OnClickListener() {
+            boolean isScreenLocked = false;
+
+            @Override
+            public void onClick(View v) {
+                if (isScreenLocked) {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                    lockRotationButton.setColorFilter(getResources().getColor(R.color.white));
+                } else {
+                    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LOCKED);
+                    lockRotationButton.setColorFilter(getResources().getColor(R.color.purple_200));
+                }
+                isScreenLocked = !isScreenLocked;
+            }
+        });
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        /* hide notification bar and set full screen. Make it again, because this settings reset
+         * after switching focus
+         */
+        if (hasFocus) {
+            playerView.setSystemUiVisibility(hideBarsFlags);
+        }
     }
 
     @Override
@@ -74,17 +140,17 @@ public class ExoPlayerActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        /* hide notification bar and set full screen. Make it again, because this settings reset
-         * after switching apps
-         */
-        playerView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | // hide navigation line on the bottom of screen (that helps go home)
-                View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY /* hide notification bar and show it only after sliding down,
-                                                                                         and hide again in some seconds */
-        );
+    protected void onPause() {
+        super.onPause();
+        player.setPlayWhenReady(false);
     }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        player.setPlayWhenReady(true);
+    }
+
 
     @Override
     protected void onDestroy() {
