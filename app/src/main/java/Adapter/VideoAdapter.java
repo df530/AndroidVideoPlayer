3aca49ed288181d38b918e20120fa41119dabb2d
AlinaUsh <alya_ush@mail.ru>
2021-05-26 15:17:19 +0300

    fix info popup window

--- app/src/main/java/Adapter/VideoAdapter.java
+++ app/src/main/java/Adapter/VideoAdapter.java
@@ -41,6 +41,9 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
     private Activity activity;
     private static String displayMode;
 
+    private boolean anyPopupOpened = false;
+    private PopupWindow lastPopupOpened;
+
     public VideoAdapter(Context context, ArrayList<VideoModel> arrayListVideos, Activity activity, String displayMode) {
         this.context = context;
         this.arrayListVideos = arrayListVideos;
@@ -91,43 +94,59 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
         }
     }
 
+    private void showPopupWindow(PopupWindow popupWindow, View popupView, int x, int y) {
+        if (anyPopupOpened) {
+            lastPopupOpened.dismiss();
+        }
+        anyPopupOpened = true;
+        lastPopupOpened = popupWindow;
+        popupWindow.showAtLocation(activity.findViewById(R.id.fragment_container), Gravity.CENTER_VERTICAL, x, y);
+    }
+
+    private boolean processOnClickInfoItem(String currentVideoLink) {
+        View popupView = LayoutInflater.from(activity).inflate(R.layout.popup_info, null);
+        popupView.setFocusable(true);
+
+        TextView videoNameTextView = popupView.findViewById(R.id.video_name);
+        videoNameTextView.setText(getVideoName(currentVideoLink));
+
+        TextView videoSizeTextView = popupView.findViewById(R.id.video_size);
+        videoSizeTextView.setText(getFileSizeMegaBytes(currentVideoLink));
+
+        TextView videoDurationTextView = popupView.findViewById(R.id.video_duration);
+        videoDurationTextView.setText(getVideoDuration(currentVideoLink));
+
+        PopupWindow popupWindow = new PopupWindow(
+                popupView,
+                WindowManager.LayoutParams.MATCH_PARENT,
+                WindowManager.LayoutParams.WRAP_CONTENT
+        );
+
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                anyPopupOpened = false;
+                return true;
+            }
+        });
+
+        showPopupWindow(popupWindow, popupView, 0, 0);
+        return true;
+    }
+
+    private boolean processOnClickRenameItem(String currentVideoLink) {
+        //TODO
+        return true;
+    }
+
     private boolean onOptionsItemSelected(@NonNull MenuItem item, @NonNull String currentVideoLink) {
         switch (item.getItemId()) {
             case R.id.video_info_item:
-                //TODO: move to a separate function
-                View popupView = LayoutInflater.from(activity).inflate(R.layout.popup_info, null);
-                popupView.setFocusable(true);
-
-                TextView videoNameTextView = popupView.findViewById(R.id.video_name);
-                videoNameTextView.setText(getVideoName(currentVideoLink));
-
-                TextView videoSizeTextView = popupView.findViewById(R.id.video_size);
-                videoSizeTextView.setText(getFileSizeMegaBytes(currentVideoLink));
-
-                TextView videoDurationTextView = popupView.findViewById(R.id.video_duration);
-                videoDurationTextView.setText(getVideoDuration(currentVideoLink));
-
-                PopupWindow popupWindow = new PopupWindow(
-                        popupView,
-                        WindowManager.LayoutParams.MATCH_PARENT,
-                        WindowManager.LayoutParams.WRAP_CONTENT
-                );
-
-                popupView.setOnTouchListener(new View.OnTouchListener() {
-                    @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        popupWindow.dismiss();
-                        return true;
-                    }
-                });
-
-                popupWindow.showAsDropDown(popupView, 0, 0);
-
-                return true;
-                    /*
-                    case R.id.video_rename_item:
-                        return true;
-                    case R.id.video_delete_item:
+                return processOnClickInfoItem(currentVideoLink);
+            case R.id.video_rename_item:
+                return processOnClickRenameItem(currentVideoLink);
+                    /*case R.id.video_delete_item:
                         return true;
                      */
             default:
@@ -184,7 +203,7 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.ViewHolder> 
     }
 
     private String getVideoDuration(String path) {
-        Uri uri =  Uri.parse(path);
+        Uri uri = Uri.parse(path);
         MediaPlayer mp = MediaPlayer.create(context, uri);
         int duration = mp.getDuration();
         mp.release();
